08520946b642e453be7623c929debf1da46a63d6
Cristiano23 <cristyano-crys@hotmail.com>
2020-01-23 09:43:48 -0200

    Finalizar segund_app

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,2 +1,9 @@
 class Micropost < ApplicationRecord
+    belongs_to :user
+    validates :content, length: { maximum: 140 }
+    algum_micropost = primeiro_usuario.microposts.first
+    algum_micropost.user
+    validates :presence, :true
+    validates :name
+    validates :email
 end
